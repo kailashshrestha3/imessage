a8f15057b83005efe22ef1c1c871e16ba13c7602
kailashshrestha3 <clasherkailash123@gmail.com>
2026-07-19 15:33:47 +0545

    feat: implement Clerk webhook handler to sync user data with MongoDB

--- backend/src/webhooks/clerk.webhook.js
+++ backend/src/webhooks/clerk.webhook.js
@@ -1,6 +1,6 @@
 import express from "express";
-import User from "../models/user.model.js"
 import { verifyWebhook } from "@clerk/backend/webhooks"
+import User from "../models/user.model.js";
 
 const router = express.Router();
 
