48e6a47219ad145534b29c86bc4d45ed6fa966ed
kailashshrestha3 <clasherkailash123@gmail.com>
2026-07-21 18:34:40 +0545

    fix: correct import paths and improve error handling in middleware and routes

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,8 +1,9 @@
-import express from "express"
-import { checkAuth } from "../controllers/auth.controller";
+import express from "express";
+import { checkAuth } from "../controllers/auth.controller.js";
+import { protectRoute } from "../../middleware/auth.middleware.js";
 
 const router = express.Router();
 
 router.get("/check", protectRoute, checkAuth);
 
-export default router
+export default router;
--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,14 +1,19 @@
 import express from "express";
-import { protecteRoute } from "../middleware/auth.middleware.js";
-import { getConversationsForSideBar, getMessages, getUsersForSidebar, sendMessage } from "../controllers/message.controller.js";
+import { protectRoute } from "../../middleware/auth.middleware.js";
+import {
+  getConversationsForSideBar,
+  getMessages,
+  getUsersForSidebar,
+  sendMessage,
+} from "../controllers/message.controller.js";
 
 import { upload } from "../../middleware/upload.middleware.js";
 
 const router = express.Router();
 
-router.get("/users", protecteRoute, getUsersForSidebar)
-router.get("/conversations", protecteRoute, getConversationsForSideBar);
-router.get("/:id", protecteRoute, getMessages);
-router.get("/send/:id", protecteRoute, upload.single("media"), sendMessage);
+router.get("/users", protectRoute, getUsersForSidebar);
+router.get("/conversations", protectRoute, getConversationsForSideBar);
+router.get("/:id", protectRoute, getMessages);
+router.get("/send/:id", protectRoute, upload.single("media"), sendMessage);
 
 export default router;
